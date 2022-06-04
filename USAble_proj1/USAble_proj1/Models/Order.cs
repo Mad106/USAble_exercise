a38fb63d99035da7a93dff8ffd0d3a8689d23920
Mad106 <msayles123@gmail.com>
2022-06-04 13:04:54 -0400

    comments added to code

--- USAble_proj1/USAble_proj1/Models/Order.cs
+++ USAble_proj1/USAble_proj1/Models/Order.cs
@@ -5,10 +5,11 @@ namespace USAble_proj1.Models
     {
         public int ID { get; set; }
         public string DateTime { get; set; } = string.Empty;
-        public string? Name { get; set; }    // updated db.Order to set Name as VARCHAR(51) (25 first name + 25 last name + 1 space)
-        public bool Closed { get; set; } = false;
-        public string? Discount { get; set; } = string.Empty;
-        public double DiscountNum { get; set; } = 0;
+        public string? Name { get; set; }    // updated dbo.Order to set Name as VARCHAR(51) (25 first name + 25 last name + 1 space)
+                                                // also have conditional in script of create.cshtml to enforce
+        public bool Closed { get; set; } = false;   // orders that are closed can not be edited and are only seen in all orders page
+        public string? Discount { get; set; } = string.Empty;   // name/type of discount
+        public double DiscountNum { get; set; } = 0;    // monetary amount of discount relative to subtotal
         public double Subtotal { get; set; } = 0;
         public double PreTax { get; set; } = 0;
         public double TaxNum { get; set; } = 0;
